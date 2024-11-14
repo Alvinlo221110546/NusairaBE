6bd105dd1954178565b554e022a8b1f069601d24
Egi Danuarta <88468631+praditus343@users.noreply.github.com>
2024-11-14 22:18:18 +0700

    test

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const port = 3020;
+const port = 3306;
 
 
 app.use(express.json());  
