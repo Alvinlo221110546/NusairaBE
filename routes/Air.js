d6a92f79abb82615e199b42d70012a7b0b1947cf
Alvinlo221110546 <221110546@students.mikroskil.ac.id>
2024-11-20 15:36:00 +0700

    fx

--- routes/Air.js
+++ routes/Air.js
@@ -3,10 +3,10 @@ import KualitasAirController from '../controller/AirController.js';
 
 const router = express.Router();
 
-router.post('/kualitas-air', KualitasAirController.addKualitasAir);
-router.get('/kualitas-air', KualitasAirController.getAllKualitasAir);
-router.get('/kualitas-air/:id', KualitasAirController.getKualitasAirById);
-router.put('/kualitas-air/:id', KualitasAirController.updateKualitasAir);
-router.delete('/kualitas-air/:id', KualitasAirController.deleteKualitasAir);
+router.post('/air', KualitasAirController.addKualitasAir);
+router.get('/air', KualitasAirController.getAllKualitasAir);
+router.get('/air/:id', KualitasAirController.getKualitasAirById);
+router.put('/air/:id', KualitasAirController.updateKualitasAir);
+router.delete('/air/:id', KualitasAirController.deleteKualitasAir);
 
 export default router;
