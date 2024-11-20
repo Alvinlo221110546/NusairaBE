804506417276ce4c1a4638a4b6c85b65066aaed2
Alvinlo221110546 <221110546@students.mikroskil.ac.id>
2024-11-20 13:42:24 +0700

    fix

--- models/DataKematian.js
+++ models/DataKematian.js
@@ -8,17 +8,15 @@ class Kematian {
         this.jumlah_ekor = data.jumlah_ekor || null;
         this.total_berat = data.total_berat || null;
         this.multiplier = data.multiplier;
-        this.size = this.calculateSize();  
+        this.size = this.calculateSize();
     }
 
-    
     calculateSize() {
         if (this.total_berat && this.multiplier) {
             return this.total_berat * this.multiplier;
         }
-        return null;  
+        return null;
     }
-
 
     static async validate(data) {
         const errors = [];
@@ -39,79 +37,63 @@ class Kematian {
             errors.push("Hanya salah satu dari Jumlah Ekor atau Total Berat yang boleh diisi.");
         }
 
-        // if (data.multiplier <= 0) {
-        //     errors.push("Multiplier harus lebih dari 0.");
-        // }
-
         return errors;
     }
 
     static async save(data) {
-        return new Promise((resolve, reject) => {
-            Kematian.validate(data).then((validationErrors) => {
-                if (validationErrors.length > 0) {
-                    return reject(new Error(validationErrors.join(", ")));
-                }
-                let size = 0;
-                if (data.total_berat && data.multiplier) {
-                    if (data.multiplier <= 0) {
-                        return reject(new Error("Multiplier tidak boleh lebih dari nol."));
-                    }
-                    size = data.total_berat * data.multiplier;
-                }
+        try {
+            const validationErrors = await Kematian.validate(data);
+            if (validationErrors.length > 0) {
+                throw new Error(validationErrors.join(", "));
+            }
 
-                db.query(
-                    'INSERT INTO kematian (kolam_id, tanggal_tebar, umur, jumlah_ekor, total_berat, multiplier, size) VALUES (?, ?, ?, ?, ?, ?, ?)',
-                    [
-                        data.kolam_id,
-                        data.tanggal_tebar,
-                        data.umur,
-                        data.jumlah_ekor,
-                        data.total_berat,
-                        data.multiplier,
-                        size  
-                    ],
-                    (err, result) => {
-                        if (err) {
-                            return reject(err);
-                        }
-                        
-                        resolve({
-                            ...data,
-                            size: size,  
-                            id: result.insertId
-                        });
-                    }
-                );
-            }).catch((error) => reject(error));
-        });
+            let size = 0;
+            if (data.total_berat && data.multiplier) {
+                if (data.multiplier <= 0) {
+                    throw new Error("Multiplier tidak boleh lebih dari nol.");
+                }
+                size = data.total_berat * data.multiplier;
+            }
+
+            const query = 'INSERT INTO kematian (kolam_id, tanggal_tebar, umur, jumlah_ekor, total_berat, multiplier, size) VALUES (?, ?, ?, ?, ?, ?, ?)';
+            const params = [
+                data.kolam_id,
+                data.tanggal_tebar,
+                data.umur,
+                data.jumlah_ekor,
+                data.total_berat,
+                data.multiplier,
+                size
+            ];
+
+            const [result] = await db.execute(query, params);
+            return {
+                ...data,
+                size: size,
+                id: result.insertId
+            };
+        } catch (error) {
+            throw error;
+        }
     }
 
-    static getAll() {
-        return new Promise((resolve, reject) => {
-            db.query('SELECT * FROM kematian', (err, result) => {
-                if (err) {
-                    return reject(err);
-                }
-                resolve(result);
-            });
-        });
+    static async getAll() {
+        try {
+            const [result] = await db.execute('SELECT * FROM kematian');
+            return result;
+        } catch (err) {
+            throw err;
+        }
     }
 
-
-    static getById(id) {
-        return new Promise((resolve, reject) => {
-            db.query('SELECT * FROM kematian WHERE id = ?', [id], (err, result) => {
-                if (err) {
-                    return reject(err);
-                }
-                resolve(result[0]);
-            });
-        });
+    static async getById(id) {
+        try {
+            const [result] = await db.execute('SELECT * FROM kematian WHERE id = ?', [id]);
+            return result[0];
+        } catch (err) {
+            throw err;
+        }
     }
-
-  
-    
 }
 
 export default Kematian;
