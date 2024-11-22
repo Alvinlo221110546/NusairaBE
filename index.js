3cd4662d9f42fca786e47af94167f574c3d91149
Alvinlo221110546 <221110546@students.mikroskil.ac.id>
2024-11-22 17:33:58 +0700

    fix

--- index.js
+++ index.js
@@ -27,11 +27,6 @@ const port = 3020;
 app.use(express.json());  
 app.use(express.urlencoded({ extended: false })); 
 
-app.use(cors({
-  origin: 'http://localhost:5173', 
-  methods: 'GET,POST,PUT,DELETE',
-  credentials: true
-}));
 
 const corsOptions = {
   origin: '*',  
