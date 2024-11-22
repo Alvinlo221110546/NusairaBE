29d9871526d07da30602d1c2da817eb8b5accba1
Alvinlo221110546 <221110546@students.mikroskil.ac.id>
2024-11-22 17:36:03 +0700

    fix

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ const __dirname = dirname(__filename);
 const app = express();
 const port = 3020;
 
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
-app.use(express.json());  
-app.use(express.urlencoded({ extended: false })); 
 
 
 const corsOptions = {
@@ -35,6 +35,7 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Routing
 app.use('/api/tambak', tambakRoutes); 
