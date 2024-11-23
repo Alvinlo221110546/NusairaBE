e8458dd1742e64322e974b798e01a3ebddae36b5
Alvinlo221110546 <221110546@students.mikroskil.ac.id>
2024-11-23 10:45:22 +0700

    fix

--- routes/Penyakit.js
+++ routes/Penyakit.js
@@ -4,8 +4,8 @@ import PenyakitController from '../controller/PenyakitController.js';
 // Membuat router baru
 const router = express.Router();
 
-// Rute untuk membuat entry penyakit, dengan pengunggahan gambar
-router.post('/penyakit', PenyakitController.uploadMiddleware, PenyakitController.createPenyakit);
+// Rute untuk membuat entry penyakit tanpa pengunggahan gambar langsung di backend
+router.post('/penyakit', PenyakitController.createPenyakit);
 
 // Rute untuk mengambil semua entry penyakit
 router.get('/penyakit', PenyakitController.getAllPenyakit);
