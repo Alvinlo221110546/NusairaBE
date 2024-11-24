866be812cdc3ed2f8bbd3e0be42573a09494f2e8
Alvinlo221110546 <221110546@students.mikroskil.ac.id>
2024-11-24 22:17:57 +0700

    fix

--- routes/Prediction.js
+++ routes/Prediction.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getPredictions } from '../controller/PredictionController.js';
+import { getPredictions  } from '../controller/PredictionController.js';
 
 const router = express.Router();
 
 router.get('/predictions', getPredictions);
 
 export default router;
+
