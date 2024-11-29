720577ba290c993b1e4d992694bd8e75bb76bfc6
Alvinlo221110546 <221110546@students.mikroskil.ac.id>
2024-11-29 16:24:28 +0700

    fix

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(cors({
 }));
 
 const corsOptions = {
-  origin: '*',  
+  origin: 'https://nusaira.vercel.app',  
   methods: 'GET, POST, PUT, DELETE',
   allowedHeaders: 'Content-Type, Authorization',
   credentials: true
