9415af8b9268db23d3c37a32f87e109df463ccaf
Alvinlo221110546 <221110546@students.mikroskil.ac.id>
2024-11-29 16:47:28 +0700

    fix

--- routes/PenyakitLele.js
+++ routes/PenyakitLele.js
@@ -1,12 +1,14 @@
 import express from 'express';
+
 import PenyakitLeleController from '../controller/PenyakitLeleController.js';
 
 const router = express.Router();
 
-router.get('/penyakit-lele', PenyakitLeleController.getAll);
-router.get('/penyakit-lele/:id', PenyakitLeleController.getById);
-router.post('/penyakit-lele', PenyakitLeleController.save);
-router.put('/penyakit-lele/:id', PenyakitLeleController.update);
-router.delete('/penyakit-lele/:id', PenyakitLeleController.delete);
+
+router.get('/penyakit-lele', PenyakitLeleController.getAllPenyakitLele);
+router.get('/penyakit-lele/:id', PenyakitLeleController.getPenyakitLeleById);
+router.post('/penyakit-lele', PenyakitLeleController.createPenyakitLele);
+router.put('/penyakit-lele/:id', PenyakitLeleController.updatePenyakitLele);
+router.delete('/penyakit-lele/:id', PenyakitLeleController.deletePenyakitLele);
 
 export default router;
