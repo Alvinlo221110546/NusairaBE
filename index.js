be0943f453e3e6bf4cb45f74151056edadcec144
Egi Danuarta <88468631+praditus343@users.noreply.github.com>
2024-11-30 21:45:59 +0700

    login

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import tambakRoutes from './routes/TambakRoutes.js';
 import siklusRouter from './routes/MulaiSiklus.js';
 import kematianRoutes from './routes/Kematian.js';
 import dataPakanRoutes from './routes/Pakan.js';
-import dataPanenRoutes from './routes/Panen.js';
+import dataPanenRoutes from './routes/Panen.js'
 import penyakitRoutes from './routes/Penyakit.js';
 import ancoRoutes from './routes/Anco.js';
 import penyakitLeleRoutes from './routes/PenyakitLele.js';
@@ -18,38 +18,54 @@ import kualitasAirRoutes from './routes/Air.js';
 import tagihanRoutes from './routes/Tagihan.js';
 import penggunaRoutes from './routes/Pengguna.js';
 import predictionRoutes from './routes/Prediction.js';
+// import { predictionService } from './models/PredictionService.js';
 import contactRoutes from './routes/Contact.js';
-import authRoutes from './routes/AuthRoutes.js'; // Mengimpor routes auth
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const _dirname = dirname(_filename);
 
 const app = express();
 const port = 3020;
 
-// Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
-// CORS Configuration
-const corsOptions = {
-  origin: 'http://localhost:5173', // Ganti ini dengan URL frontend yang sesuai saat produksi
+app.use(express.json());  
+app.use(express.urlencoded({ extended: false })); 
+
+app.use(cors({
+  origin: 'http://localhost:5173', 
   methods: 'GET,POST,PUT,DELETE',
+  credentials: true
+}));
+
+//sementara
+const corsOptions = {
+  origin: '*',  
+  methods: 'GET, POST, PUT, DELETE',
+  allowedHeaders: 'Content-Type, Authorization',
   credentials: true
 };
 app.use(cors(corsOptions));
 
-// Static Files
+
+// const allowedOrigins = ['https://nusaira.vercel.app', 'http://localhost:5173'];
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   }
+// }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/uploads', express.static('uploads')); // Untuk mengakses file upload
 
 // Routing
-app.use('/api/tambak', tambakRoutes);
+app.use('/api/tambak', tambakRoutes); 
 app.use('/api', siklusRouter);
 app.use('/api', kematianRoutes);
 app.use('/api', penyakitRoutes);
 app.use('/api', dataPakanRoutes);
-app.use('/api', dataPanenRoutes);
+app.use('/api', dataPanenRoutes); 
 app.use('/api', ancoRoutes);
 app.use('/api', penyakitLeleRoutes);
 app.use('/api', beritaRoutes);
@@ -59,11 +75,16 @@ app.use('/api', pemasukanRoutes);
 app.use('/api', kualitasAirRoutes);
 app.use('/api', tagihanRoutes);
 app.use('/api', penggunaRoutes);
-app.use('/api', predictionRoutes);
+app.use('/api', predictionRoutes); 
+app.use('/uploads', express.static('uploads'));
 app.use('/api/contact', contactRoutes);
-app.use('/api/auth', authRoutes); // Menggunakan router auth
 
-// Server Listener
+
+//ini akan dipakai untuk prediksi
+// predictionService.startScheduler();
+
+
+
 app.listen(port, async () => {
-  console.log(`Server berjalan di http://localhost:${port}`);
+  console.log(Server berjalan di http://localhost:${port});
 });
