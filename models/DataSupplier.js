b5e46530a764c3a81e8a30aee216f64d2f5d8529
Alvinlo221110546 <221110546@students.mikroskil.ac.id>
2024-12-02 19:19:26 +0700

    fix

--- models/DataSupplier.js
+++ models/DataSupplier.js
@@ -31,13 +31,13 @@ class Supplier {
 
       const [result] = await db.execute(query, [
         supplier.supplier,
-        supplier.province, // No JSON.stringify here
-        supplier.location, // No JSON.stringify here
+        supplier.province, 
+        supplier.location, 
         supplier.description,
         supplier.image,
         supplier.availability,
         supplier.whatsapp,
-        JSON.stringify(supplier.products), // Only JSON.stringify for products
+        JSON.stringify(supplier.products),
       ]);
 
       supplier.id = result.insertId;
@@ -54,9 +54,9 @@ class Supplier {
       const [results] = await db.execute(query);
       return results.map(result => ({
         ...result,
-        province: result.province, 
-        location: result.location, 
-        products: JSON.parse(result.products), 
+        province: result.province || [], 
+        location: result.location || [], 
+        products: result.products ? JSON.parse(result.products) : [],
       }));
     } catch (error) {
       console.error('Error saat mengambil semua data Supplier:', error.message);
@@ -75,9 +75,9 @@ class Supplier {
       const result = results[0];
       return {
         ...result,
-        province: result.province, 
-        location: result.location, 
-        products: JSON.parse(result.products), 
+        province: result.province || [], 
+        location: result.location || [], 
+        products: result.products ? JSON.parse(result.products) : [],
       };
     } catch (error) {
       console.error('Error saat mengambil data Supplier berdasarkan ID:', error.message);
@@ -102,7 +102,7 @@ class Supplier {
         data.image,
         data.availability,
         data.whatsapp,
-        JSON.stringify(data.products), 
+        JSON.stringify(data.products),
         id,
       ]);
 
