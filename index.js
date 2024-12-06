868af92bce5c545bd9eed74bc26765232fde8903
Ogya Rajendra <ogyarjndra@gmail.com>
2024-12-07 01:06:49 +0700

    login balek

--- index.js
+++ index.js
@@ -37,8 +37,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false })); 
 
 app.use(cors({
-  // origin: 'http://localhost:5173'', 
-  origin: 'https://nusaira.vercel.app/',
+  origin: 'http://localhost:5173', 
   methods: 'GET,POST,PUT,DELETE',
   credentials: true
 }));
