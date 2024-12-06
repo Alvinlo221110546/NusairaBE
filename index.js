0cceecd9884918b89af470c903ab0a007f54a116
Ogya Rajendra <ogyarjndra@gmail.com>
2024-12-07 01:51:11 +0700

    login

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false })); 
 
 app.use(cors({
-  origin: 'https://nusaira-be.vercel.app/api/login', 
+  origin: 'http://localhost:5173', 
   methods: 'GET,POST,PUT,DELETE',
   credentials: true
 }));
