d68f766c55eb1c2a19cc13f0abd5a44135d7df6e
Alvinlo221110546 <221110546@students.mikroskil.ac.id>
2024-12-12 09:36:07 +0700

    auth tambak

--- routes/TambakRoutes.js
+++ routes/TambakRoutes.js
@@ -1,19 +1,20 @@
 import express from 'express';
 import TambakController from '../controller/TambakController.js';
+import authMiddleware from '../middleware/authMiddleware.js'; 
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', authMiddleware, (req, res) => {
     console.log('Request body:', req.body); 
     TambakController.addTambak(req, res);
 });
 
-router.get('/', TambakController.getAllTambak);
-router.get('/:id', TambakController.getTambakById);
-router.put('/:id', (req, res) => {
+router.get('/', authMiddleware, TambakController.getAllTambak);
+router.get('/:id', authMiddleware, TambakController.getTambakById);
+router.put('/:id', authMiddleware, (req, res) => {
     TambakController.updateTambak(req, res);
 });
-router.delete('/:id', (req, res) => {
+router.delete('/:id', authMiddleware, (req, res) => {
     TambakController.deleteTambak(req, res);
 });
 
