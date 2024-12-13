f84b16ea825a74d30b1b981d2e54d62d59ede488
Alvinlo221110546 <221110546@students.mikroskil.ac.id>
2024-12-13 20:00:23 +0700

    favorite

--- controller/FavoriteController.js
+++ controller/FavoriteController.js
@@ -8,22 +8,18 @@ class FavoriteController {
       
       console.log("Parameter diterima:", { user_id, buku_id });
     
-      // Validasi input buku_id dan user_id
       if (!buku_id || user_id === null) {
         console.error("Buku ID atau User ID tidak valid:", { buku_id, user_id });
         return res.status(400).json({ message: "Buku ID atau User ID tidak valid" });
       }
-  
-      // Menambahkan ke favorit
+
       const favorite = await Favorite.create({ buku_id, user_id });
   
-      // Mengembalikan respons jika berhasil
       res.status(200).json({
         message: 'Buku berhasil ditambahkan ke favorit',
         favorite
       });
     } catch (error) {
-      // Menangani error
       if (error.message.includes('sudah ada di daftar favorit')) {
         res.status(400).json({ message: error.message });
       } else {
