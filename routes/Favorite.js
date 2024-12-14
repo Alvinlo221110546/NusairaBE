8b36fcf1ffdb510585c5374b7110d3b52a652415
Alvinlo221110546 <221110546@students.mikroskil.ac.id>
2024-12-14 09:49:31 +0700

    sedikit perbaikan

--- routes/Favorite.js
+++ routes/Favorite.js
@@ -5,6 +5,7 @@ import authMiddleware from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.post('/favorites', authMiddleware, FavoriteController.addToFavorites);
+router.get('/favorites/all', authMiddleware, FavoriteController.getAllFavorites);
 router.get('/favorites', authMiddleware, FavoriteController.getFavorites);
 router.delete('/favorites/:buku_id', authMiddleware, FavoriteController.removeFavorite);
 
