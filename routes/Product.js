cec25b8d5e40e317b9868a7f5a9f4a2c87a362a2
Alvinlo221110546 <221110546@students.mikroskil.ac.id>
2024-12-16 19:42:00 +0700

    fix

--- routes/Product.js
+++ routes/Product.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import ProductController from '../controller/ProductController.js';
 import adminAuthMiddleware from '../middleware/adminAuthMiddleware.js'; 
+import authMiddleware from "../middleware/authMiddleware.js";
 
 const productRoutes = express.Router();
 
 productRoutes.post('/',adminAuthMiddleware, ProductController.createProduct);
-productRoutes.get('/:supplierId', ProductController.getProductsBySupplierId);
-productRoutes.get('/', ProductController.getAllProducts);  
+productRoutes.get('/:supplierId',adminAuthMiddleware, ProductController.getProductsBySupplierId);
+productRoutes.get('/',adminAuthMiddleware, ProductController.getAllProducts);  
+productRoutes.get('/:supplierId',authMiddleware, ProductController.getProductsBySupplierId);
+productRoutes.get('/',authMiddleware, ProductController.getAllProducts); 
 productRoutes.put('/:id',adminAuthMiddleware, ProductController.updateProduct);
 productRoutes.delete('/:id',adminAuthMiddleware, ProductController.deleteProduct);
 
