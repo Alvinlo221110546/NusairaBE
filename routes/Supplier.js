67377af5b2da9ed5dfb0e19c414e4d76b698eb1f
Alvinlo221110546 <221110546@students.mikroskil.ac.id>
2024-12-16 19:44:45 +0700

    fix

--- routes/Supplier.js
+++ routes/Supplier.js
@@ -1,17 +1,14 @@
 import express from 'express';
 import SupplierController from '../controller/SupplierController.js';
 import adminAuthMiddleware from '../middleware/adminAuthMiddleware.js'; 
-import authMiddleware from "../middleware/authMiddleware.js";
 
 const supplierRoutes = express.Router();
 
 
 supplierRoutes.post('/', adminAuthMiddleware, SupplierController.createSupplier);
-supplierRoutes.get('/',adminAuthMiddleware, SupplierController.getAllSuppliers);
-supplierRoutes.get('/:id',adminAuthMiddleware, SupplierController.getSupplierById);
-supplierRoutes.get('/',authMiddleware, SupplierController.getAllSuppliers);
-supplierRoutes.get('/:id',authMiddleware, SupplierController.getSupplierById);
-supplierRoutes.put('/:id',adminAuthMiddleware,  SupplierController.updateSupplier);
+supplierRoutes.get('/', SupplierController.getAllSuppliers);
+supplierRoutes.get('/:id', SupplierController.getSupplierById);
+supplierRoutes.put('/:id', adminAuthMiddleware, SupplierController.updateSupplier);
 supplierRoutes.delete('/:id',adminAuthMiddleware, SupplierController.deleteSupplier);
 supplierRoutes.get('/province/:province', SupplierController.getSuppliersByProvince);
 supplierRoutes.get('/location/:location', SupplierController.getSuppliersByLocation);
